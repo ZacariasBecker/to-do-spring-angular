dbd3a4e1d920fa110c59835cc74f30f3b2457714
ZacariasBecker <zacariasbecker@gmail.com>
2024-03-21 15:02:55 -0300

    TaskController

--- to-do-app/src/main/java/becker/zacarias/todoapp/controllers/TaskController.java
+++ to-do-app/src/main/java/becker/zacarias/todoapp/controllers/TaskController.java
@@ -1,5 +1,52 @@
 package becker.zacarias.todoapp.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import becker.zacarias.todoapp.models.TaskRequestDTO;
+import becker.zacarias.todoapp.models.TaskResponseDTO;
+import becker.zacarias.todoapp.repositories.TaskRepository;
+
+@RestController
+@RequestMapping("task")
 public class TaskController {
 
+	@Autowired
+	private TaskRepository taskRepository;
+
+	@GetMapping
+	public ResponseEntity<List<TaskResponseDTO>> getAll() {
+
+	}
+
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Optional<TaskResponseDTO>> getById(@PathVariable Integer id) {
+
+	}
+
+	@PostMapping
+	public ResponseEntity<TaskResponseDTO> save(@RequestBody TaskRequestDTO newData) {
+
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Optional<TaskResponseDTO>> deteleById(@PathVariable Integer id) {
+
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<TaskResponseDTO> update(@PathVariable Integer id, @RequestBody TaskRequestDTO newData) {
+
+	}
 }
