718369649e08bd766f6b15a25cf2250c3847e5bf
ZacariasBecker <zacariasbecker@gmail.com>
2024-03-22 10:06:37 -0300

    task constructor fix

--- to-do-app/src/main/java/becker/zacarias/todoapp/controllers/TaskController.java
+++ to-do-app/src/main/java/becker/zacarias/todoapp/controllers/TaskController.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import org.hibernate.event.internal.DefaultPersistOnFlushEventListener;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
