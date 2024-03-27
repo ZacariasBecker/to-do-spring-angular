0d17a2884c861781af7c8478ce3f1035f752fd22
ZacariasBecker <zacariasbecker@gmail.com>
2024-03-27 11:27:09 -0300

    dev tools

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { TaskBookComponent } from './task-book/task-book.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { DevToolsComponent } from './dev-tools/dev-tools.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TaskBookComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, TaskBookComponent, NavbarComponent, FooterComponent, DevToolsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ client/src/app/dev-tools/dev-tools.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { ITaskResponse } from '../models/i-task-response';
+import { ToDoApiService } from '../to-do-api.service';
+import { ITaskRequest } from '../models/i-task-request';
+
+@Component({
+  selector: 'app-dev-tools',
+  standalone: true,
+  imports: [],
+  templateUrl: './dev-tools.component.html',
+  styleUrl: './dev-tools.component.css',
+  providers: [
+    ToDoApiService
+  ]
+})
+export class DevToolsComponent {
+
+  tasks?: ITaskResponse[] = [];
+
+  async dtDeleteAll() {
+    this.getAllTasks();
+
+    const idList: string[] = [];
+    this.tasks!.map(e => idList.push(e.id));
+    console.log(idList);
+
+    for (const id of idList) {
+      this.deleteTask(id);
+    }
+  }
+
+
+  dtFourItems() {
+    this.getAllTasks();
+
+    this.dtDeleteAll();
+    this.dtMockPlusFour();
+  }
+
+  dtMockPlusFour() {
+    this.getAllTasks();
+
+    for (let i = 0; i < 4; i++) {
+
+      let initDate = new Date();
+      const openedDate = new Date(new Date(initDate).setDate(initDate.getDate() + i));
+      const closedDate = new Date(new Date(openedDate).setDate(openedDate.getDate() + 5));
+
+      this.postTask({
+        name: "NOME",
+        description: "DESCRIÇÃO",
+        openedDate: openedDate.toString(),
+        closedDate: closedDate.toString(),
+        completed: true
+      });
+    }
+  }
+
+  constructor(private toDoApiService: ToDoApiService) { }
+
+  ngOnInit() {
+    this.getAllTasks();
+  }
+
+  postTask(newTask: ITaskRequest) {
+    this.toDoApiService.postTask(newTask).subscribe((data: ITaskResponse) => {
+      console.log(data);
+    });
+  }
+
+  getAllTasks() {
+    this.toDoApiService.getAllTasks().subscribe((data: ITaskResponse[]) => {
+      this.tasks = data.sort((a, b) => new Date(a.openedDate).valueOf() - new Date(b.openedDate).valueOf());
+    });
+  }
+
+  deleteTask(id: string) {
+    this.toDoApiService.deteleTask(id).subscribe((data: ITaskResponse) => {
+      console.log(data);
+    });
+  }
+
+
+}
